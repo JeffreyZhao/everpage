6893307b31b5c43650bbf92b965503b71a21be41
Jeffrey Zhao <jeffz@live.com>
2013-03-07 23:16:36 +0800

    rename note to Note for view folder and route it correctly

--- src/Everpage/Global.asax.cs
+++ src/Everpage/Global.asax.cs
@@ -20,11 +20,17 @@
             routes.IgnoreRoute("index.html");
 
             routes.MapRoute(
+                "Note.Index",
+                "note",
+                new { controller = "Note", action = "Index" });
+
+            /*
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
                 );
-
+             */
         }
 
         protected void Application_Start() {
